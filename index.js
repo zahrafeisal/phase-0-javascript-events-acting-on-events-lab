9347722e8de13c810926a4363614e1d79dd113c0
zahrafeisal <zahrafeisal3@gmail.com>
2024-06-27 16:49:17 +0300

    added moveDodgerLeft and moveDodgerRight

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+  let left = dodger.style.left.replace("px", "");
+  function moveDodgerRight() {
+    let newPosition = parseInt(left, 10);
+
+    if (left < 360) {
+      dodger.style.left = `${newPosition + 1}px`;
+    }
+}
+
